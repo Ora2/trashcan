10c50e8f57f1040bb2ecbb54299282cf4569033f
lyd <984689593@qq.com>
2020-08-23 12:28:31 +0800

    初始化

---
+++ src/main/java/training/io/trashcan/TrashcanApplication.java
@@ -0,0 +1,13 @@
+package training.io.trashcan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashcanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrashcanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/training/io/trashcan/TrashcanApplicationTests.java
@@ -0,0 +1,13 @@
+package training.io.trashcan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashcanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
